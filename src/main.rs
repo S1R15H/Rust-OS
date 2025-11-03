0e6693d0b19c268323323245503c7e78ec4dccb5
Sirish Gurung <sirishgurung112@gmail.com>
2025-11-03 12:47:04 -0500

    VGA Text configured

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,17 @@ use core::panic::PanicInfo;
 /// This function is called on panic.
 /// PanicInfo contains the file and line where panic happened.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)] // Disable name mangling to ensure the function name is preserved
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
     loop{}
 }
+
+mod vga_buffer;
