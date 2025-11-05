391c0bbd741ef299132c0c95fbbb323a4e3d306c
Sirish Gurung <sirishgurung112@gmail.com>
2025-11-05 09:29:28 -0500

    Added Breakpoint exception handler

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,16 @@ use os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    os::init(); 
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3(); 
+
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
