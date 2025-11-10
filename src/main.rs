c71c40c4d4ec11ab3aba861635fc5650aeb9060f
Sirish Gurung <sirishgurung112@gmail.com>
2025-11-10 17:05:15 -0500

    Tested heap implementation

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 use os::println;
 
 use bootloader::{BootInfo, entry_point};
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+
+extern crate alloc;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use os::memory;
-    use x86_64::{structures::paging::Page, VirtAddr};
-    use os::memory::BootInfoFrameAllocator;
+    use os::allocator; // new import
+    use os::memory::{self, BootInfoFrameAllocator};
 
     
     println!("Hello World{}", "!");
@@ -25,14 +28,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe {
             BootInfoFrameAllocator::init(&boot_info.memory_map)
         };
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
 
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+    
     // as before
     #[cfg(test)]
     test_main();
@@ -54,3 +71,6 @@ fn panic(info: &PanicInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
 }
+
+
+
