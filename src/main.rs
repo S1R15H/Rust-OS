d8c814a826f24865b6c62d418dadbf45bc9e74b3
Sirish Gurung <sirishgurung112@gmail.com>
2025-11-15 23:47:09 -0500

    Multitasking implementation

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@
 use core::panic::PanicInfo;
 use x86_64::VirtAddr;
 use os::println;
-
+use os::task::keyboard;
 use bootloader::{BootInfo, entry_point};
 use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use os::task::{Task, simple_executor::SimpleExecutor};
+use os::task::executor::Executor;
 
 extern crate alloc;
 
@@ -31,28 +33,15 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
-
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
     
     // as before
     #[cfg(test)]
     test_main();
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses())); 
+    executor.run();
 
     println!("It did not crash!");
     os::hlt_loop();
@@ -66,11 +55,18 @@ fn panic(info: &PanicInfo) -> ! {
     os::hlt_loop();
 }
 
-#[cfg(test)]
+#[cfg(test)]      
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
 }
 
+async fn async_number() -> u32 {
+    42
+}
 
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
